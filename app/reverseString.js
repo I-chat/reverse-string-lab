b24b9fce10cdcb365ce5a5f87b7af57ddf500852
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-10 04:31:29 +0100

    added comments to app

--- app/reverseString.js
+++ app/reverseString.js
@@ -1,16 +1,16 @@
 var reverseString = function (str) {
-	var reverse = '';
+	var reverse = ''; // empty string to hold and add reversed letters
 	if (str === '') {
-		return null;
+		return null;// returns null for empty str
 	} else {
-		for (var i = str.length - 1; i >= 0; i--) {
-			reverse += str[i]
+		for (var i = str.length - 1; i >= 0; i--) { // reducing for loop
+			reverse += str[i]// assigns and adds letters from the back
 		}
 	}
 	if (str === reverse) {
-		return true;
+		return true; // returns true if str and reverse are equal
 	}
-	return reverse;
+	return reverse; //returns reverse
 }
 
 module.exports = reverseString
